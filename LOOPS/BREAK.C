993c9c6c3fface5159926cde9e53ccf0df8f6b6b
shah basit <bas019829@iust.ac.in>
2026-07-08 23:35:31 +0530

    Update code with recent changes

--- LOOPS/BREAK.C
+++ LOOPS/BREAK.C
@@ -17,16 +17,21 @@
 //check a number is prime or not:
 #include <stdio.h>
 int main() {
-    int n;
+    int n,i;
     printf("Enter your number:");
     scanf("%d",&n);
-    for(int i=2;i<n-1;i++) {
+    for( i=2;i<n;i++) {
         if(n%i==0){
             printf("The given number is composite:");
             break; //if we dont use break and some no. will have more than one multiples it will print this thing that times .
-           
+            }
         }
-    }
+            if(i==n)    {
+                printf("The number is Prime.");
+            }
+        
+       
+        
     return 0;
 
 }
--- LOOPS/continue_statemnt.c
+++ LOOPS/continue_statemnt.c
@@ -1,18 +1,18 @@
-// #include <stdio.h>
-// int main() {
-//   for (int i=1; i<=100; i++ ) {
-//     if(i % 2 ==1){
-//     continue;    //means skip kro is round ko.
-//   } 
-//     printf("%d\n ",i);
+#include <stdio.h>
+int main() {
+  for (int i=1; i<=100; i++ ) {
+    if(i % 2 ==1){
+    continue;    //means skip kro is round ko.
+  } 
+    printf("%d\n ",i);
      
-//   }
-//   return 0;
-// }
+  }
+  return 0;
+}
 
 
 
 
 
-//
+
 
